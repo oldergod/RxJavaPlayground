ab80b0acac7e112bd543531baa1608a677cbb11f
Benoit Quenaudon <bquenaudon@squareup.com>
2019-08-08 11:27:38 -0400

    updating

--- src/main/java/play/RxPlay.kt
+++ src/main/java/play/RxPlay.kt
@@ -1,4 +1,18 @@
 package play
 
-fun main(args: Array<String>) {
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+
+fun main() {
+  val a = PublishSubject.create<Int>()
+  val b = PublishSubject.create<Int>()
+  val c = PublishSubject.create<Int>()
+
+  Observable.merge(listOf(a, b, c).map { it.map { it * 3 } })
+      .subscribe { println(it) }
+
+  a.onNext(3)
+  b.onNext(2)
+  c.onNext(6)
+  a.onNext(1)
 }
